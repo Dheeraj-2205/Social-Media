c1a8a7a0f677df06cd13b1b5e4541df6a055c213
Dheeraj-2205 <djsays124@gmail.com>
2023-03-17 21:01:45 +0530

    Working login part in backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken")
 const userSchema = new mongoose.Schema({
     name : {
         type : String,
@@ -51,6 +51,10 @@ userSchema.pre("save", async function(next){
 
 userSchema.methods.matchPassword = async function (password){
     return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateToken = function (){
+JWT_SECRET = "KDFLAKJRJOREFKAKDKALFDJLFJEOIRIOW"
+    return jwt.sign({_id : this._id},process.env.JWT_SECRET)
 }
 
 module.exports = mongoose.model("User",userSchema);
