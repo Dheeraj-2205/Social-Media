ee901ac3d00018ea7d86c76b37899ea25993b528
Dheeraj-2205 <djsays124@gmail.com>
2023-03-21 20:16:17 +0530

    error spotted in updateCaption part

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {createPost,likeAndUnlikePost,deletePost} =require("../controllers/post");
+const {createPost,likeAndUnlikePost,deletePost, updateCaption} =require("../controllers/post");
 const { getPostOfFollowing } = require("../controllers/user");
 const {isAuthenticated} = require("../middlewares/auth")
 const router = express.Router();
@@ -9,6 +9,7 @@ router.route("/post/upload").post(isAuthenticated,createPost);
 router
     .route("/post/:id")
     .get(isAuthenticated,likeAndUnlikePost)
+    .put(isAuthenticated,updateCaption)
     .delete(isAuthenticated,deletePost);
     
 router.route("/posts").get(isAuthenticated,getPostOfFollowing)
