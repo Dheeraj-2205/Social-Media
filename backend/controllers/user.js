12eaad910876f738cd02aad4fd51bbf97a0ecdac
Dheeraj-2205 <djsays124@gmail.com>
2023-03-21 14:54:39 +0530

    updated Profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -204,3 +204,14 @@ exports.updatePassword = async (req,res)=>{
         })
     }
 }
+
+exports.updateProfile = async(req,res) =>{
+    try {
+        const user = await User.findById(req.user._id);
+    } catch (error) {
+        res.status(500).json({
+            sucesss : false,
+            message : error.message
+        })
+    }
+}
