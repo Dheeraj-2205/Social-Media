9c936852a7b7f047f8fcacfade13bf0b2b14e2de
Dheeraj-2205 <djsays124@gmail.com>
2023-03-21 15:19:30 +0530

    hash arguments required in password

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,6 +7,6 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout)
 router.route("/follow/:id").get(isAuthenticated,followUser)
-router.route("/upadate/password").put(isAuthenticated,updatePassword)
-router.route("/upadate/profile").put(isAuthenticated,updateProfile)
+router.route("/update/password").put(isAuthenticated,updatePassword)
+router.route("/update/profile").put(isAuthenticated,updateProfile)
 module.exports = router;
