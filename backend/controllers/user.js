6624ae397b38d117f2f19f1410aad7f8389b6071
Dheeraj-2205 <djsays124@gmail.com>
2023-03-21 15:03:07 +0530

    profile is updated

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -208,6 +208,21 @@ exports.updatePassword = async (req,res)=>{
 exports.updateProfile = async(req,res) =>{
     try {
         const user = await User.findById(req.user._id);
+
+        const {name,email} = req.body;
+
+        if(name){
+            user.name = name;
+        }
+        if(email) {
+            user.email = email;
+        }
+
+        await user.save();
+        res.status(200).json({
+            success : true,
+            message :"Profile Updated"
+        })
     } catch (error) {
         res.status(500).json({
             sucesss : false,
