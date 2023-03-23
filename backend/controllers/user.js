80895d9096a870d484545f60a5e637c372749502
Dheeraj-2205 <djsays124@gmail.com>
2023-03-23 13:37:49 +0530

    get my profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -254,4 +254,19 @@ exports.deleteMyProfile = async (req, res) => {
   }
 };
 
+exports.myProfile = async(req,res)=>{
+    try {
+        const user = await User.findById(req.user._id).populate("posts");
 
+        res.status(200).json({
+            sucess : true,
+            user,
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : error.message
+        })
+    }
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {register,login, followUser, logout, updatePassword, updateProfile, deleteMyProfile} = require("../controllers/user")
+const {register,login, followUser, logout, updatePassword, updateProfile, deleteMyProfile, myProfile} = require("../controllers/user")
 const {isAuthenticated} = require("../middlewares/auth")
 const router = express.Router();
 
@@ -10,4 +10,5 @@ router.route("/follow/:id").get(isAuthenticated,followUser)
 router.route("/update/password").put(isAuthenticated,updatePassword)
 router.route("/update/profile").put(isAuthenticated,updateProfile)
 router.route("/delete/me").delete(isAuthenticated,deleteMyProfile)
+router.route("/me").get(isAuthenticated,myProfile)
 module.exports = router;
