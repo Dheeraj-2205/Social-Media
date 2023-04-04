46a642cea358c2f0cfb1e77dd3ebb932a347df47
Dheeraj-2205 <djsays124@gmail.com>
2023-04-05 01:53:34 +0530

    mongoose update

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -10,7 +10,7 @@ const postSchema = new mongoose.Schema({
         type : mongoose.Schema.Types.ObjectId,
         ref : "User"
     },
-
+    
     createdAt :{
         type : Date,
         default : Date.now
