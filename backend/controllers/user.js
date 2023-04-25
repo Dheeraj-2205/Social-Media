04c67f753c326eedd1b9a3051a6dabb2097c5ab4
Dheeraj-2205 <djsays124@gmail.com>
2023-04-25 23:50:54 +0530

    adding sendmail

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const Post = require("../models/Post");
 const User = require("../models/User");
+const {sendEmail} = require("../middlewares/sendEmail") 
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
