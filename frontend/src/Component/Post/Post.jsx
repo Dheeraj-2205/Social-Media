e74488c52e7ccc1cae201ef197be8196bd6d000e
Dheeraj-2205 <djsays124@gmail.com>
2023-10-17 22:36:19 +0530

    like the part

--- frontend/src/Component/Post/Post.jsx
+++ frontend/src/Component/Post/Post.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Post.css";
 import { Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
 const Post = ({
   postId,
   caption,
@@ -13,21 +13,31 @@ const Post = ({
   ownerName,
   ownerId,
   isDelete = false,
-  isAccount = false
+  isAccount = false,
 }) => {
-  return <div className="post">
-    <div className="postHeader"></div>
-        <img src={postImage} alt="post" />
-        <div className="postDetails">
-            <Avatar src = {ownerImage} alt = "User" sx = {{
-                height : '3vmax',
-                width : "3vmax"
-            }} />
-            <Link to = { `/user/${ownerId}`}>
-                <Typography fontWeight = {700}>{ownerName}</Typography>
-            </Link>
-        </div>
-  </div>;
+  return (
+    <div className="post">
+      <div className="postHeader"></div>
+      <img src={postImage} alt="post" />
+      <div className="postDetails">
+        <Avatar
+          src={ownerImage}
+          alt="User"
+          sx={{
+            height: "3vmax",
+            width: "3vmax",
+          }}
+        />
+        <Link to={`/user/${ownerId}`}>
+          <Typography fontWeight={700}>{ownerName}</Typography>
+        </Link>
+        <Typography style={{ alignSelf: "center" }}>{caption}</Typography>
+      </div>
+      <button className="like-button">
+        <Typography>5 Likes</Typography>
+      </button>
+    </div>
+  );
 };
 
 export default Post;
